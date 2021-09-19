8bcb51550708869383d0d0a87f9c8ac104ac0f78
shirai <cpaichiro@gmail.com>
2021-09-19 15:08:58 +0900

    create test.py

--- test.py
+++ test.py
@@ -1,1 +1,27 @@
-print("test1")
+# task1
+
+name1 : str = "ｼﾝｼﾞ"
+name2 : str = "ｶｦﾙくん"
+
+print(name1 + "と" + name2 + "は仲良し")
+
+
+# task2
+
+name3 : str = "使徒です!"
+if name3 == "使徒です!"
+print("使徒です!")
+
+
+# task3
+
+names: list =["シンジ", "レイ", "カヲルくん"]
+list.append("アスカ")
+print("list")
+
+# task4
+
+for names in["シンジ", "レイ", "カヲルくん", "アスカ"]:
+ print(names)
+
+#  task5
